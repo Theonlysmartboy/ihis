eee657030f99e48de1b8d276bda3767d7675fef2
TheOnlySmartBoy <o2jose43@gmail.com>
2019-05-16 17:47:52 +0300

    Completed adding roles and statuses seeder Begun adding more migration, models and controller files

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use Illuminate\Database\Seeder;
 
 class RolesTableSeeder extends Seeder
@@ -11,6 +12,16 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $items = [
+
+            ['id' => 1, 'rl_name' => 'User','rl_clearance' => 'Can view their patient details',],
+            ['id' => 2, 'rl_name' => 'Administrator','rl_clearance' => 'Can create other regions and add hospitals',],
+            ['id' => 3, 'rl_name' => 'Doctor','rl_clearance' => 'Can add and view patients and their details',],
+
+        ];
+
+        foreach ($items as $item) {
+            Role::create($item);
+        }
     }
 }
--- database/seeds/StatusesTableSeeder.php
+++ database/seeds/StatusesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Status;
 use Illuminate\Database\Seeder;
 
 class StatusesTableSeeder extends Seeder
@@ -11,6 +12,18 @@ class StatusesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $items = [
+
+            ['id' => 1, 'st_name' => 'Pending','st_desc' => 'Not yet confirmed',],
+            ['id' => 2, 'st_name' => 'Confirmed','st_desc' => 'Confirmed',],
+            ['id' => 3, 'st_name' => 'Cancelled','st_desc' => 'Cancelled',],
+            ['id' => 4, 'st_name' => 'Active','st_desc' => 'Is in use',],
+            ['id' => 5, 'st_name' => 'Dormant','st_desc' => 'Not in use',],
+
+        ];
+
+        foreach ($items as $item) {
+            Status::create($item);
+        }
     }
 }
